f87148c862cfb41864f029501d9382c88434e846
juanpablo45875785 <52146627+juanpablo45875785@users.noreply.github.com>
2024-03-08 09:26:45 -0400

    subido

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+static class Program
+{
+    static void Main(string[] args)
+    {
+        //Console.WriteLine("Ingrese su primera nota: ");
+        //int nota1 = int.Parse(Console.ReadLine());
+
+       // Console.WriteLine("Ingrese su segunda nota: ");
+        //int nota2 = int.Parse(Console.ReadLine());
+
+       // EvaluarNota(nota1, nota2);
+        rotar();
+        Console.ReadLine();
+    }
+    static void rotar(){
+        string numero = Console.ReadLine();
+        char primero = numero[0];
+        char segundo = numero[1];
+        string resto = numero.Substring(2, numero.Length - 2);
+        string resultado = resto + segundo + primero;
+        Console.WriteLine(resultado);
+    }
+
+    static void EvaluarNota(int nota1, int nota2)
+    {
+        int promedio = (nota1 + nota2) / 2;
+
+        if (promedio > 50)
+        {
+            Console.WriteLine("Aprobado");
+        }
+        else
+        {
+            Console.WriteLine("Reprobado");
+        }
+    }
+    
+}
