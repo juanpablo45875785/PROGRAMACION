76c400b89c3d8b8a9d9874674760f4a92752b6b4
juanpablo45875785 <52146627+juanpablo45875785@users.noreply.github.com>
2024-03-08 09:51:45 -0400

    cambio2

---
+++ clase#1/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("JUAN PABLO 123");
+Console.WriteLine("es hora de irnos");
+Console.WriteLine("hoy es miercole");
+
