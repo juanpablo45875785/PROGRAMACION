700281e66e25aee20f982ce9fba4989a432f767c
juanpablo45875785 <jtapeosichamo@gmail.com>
2024-03-12 19:47:23 -0400

    marte

--- clase marte/Program.cs
+++ clase marte/Program.cs
@@ -1,1 +1,34 @@
-﻿
+﻿using System;
+static class Program{
+    static void Main(string[] args)
+    {
+        llenarVector(6);
+    }
+    static void llenarVector(int cantidad){
+        int[] vector = new int[cantidad];
+        int numero= 1;
+        for(int i=0; i< cantidad; i++){
+            while(!espar(numero)){
+                numero++;
+            }
+            vector[i]=numero;
+            numero++;
+        }
+        imprimirVector(vector);
+    }
+    static bool espar(int numero){
+        if((numero % 2)==1){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    static void imprimirVector(int[] vector){
+        foreach(int elemento in vector){
+            Console.Write(elemento + " ");
+        }
+        Console.WriteLine();
+    }
+    
+}
